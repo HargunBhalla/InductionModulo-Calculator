348c73de14d1ca31ef90d42b0922fa3f75750c3b
Hargun Bhalla <38545675+HargunBhalla@users.noreply.github.com>
2024-04-01 20:10:49 -0400

    Create modulo_feature.py

---
+++ modulo_feature.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a value for a: "))
+b = int(input("Enter a value for b: "))
+print((f'To confirm, the value of a is: {a}')+(f' and the value of b is: {b}'))
+
+x = a % b 
+print(f'The modulo for the two values is {x}')
